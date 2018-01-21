5cd59a0adb9ea6c39a7ca9a13126dd4130b1b44d
MarcyNash <marcywsa@gmail.com>
2018-01-21 14:06:34 -0500

    Added background image, reset dialogs after use and styled nav buttons.
    Added background image from unsplash.com. Styled with css.
    
    Added padding to nav-right buttons and removed default nav
    class for better spacing.

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const store = {
+
+  // const resetForm = function (form) {
+  //   $(form).find('input:text, input:password, textarea').val('')
+  // }
 }
 
-module.exports = store
+module.exports = {
+  store
+}
